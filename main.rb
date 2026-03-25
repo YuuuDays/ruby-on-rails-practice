46858e626c109c1084c0e5913afb15b8f5989887
hikida <ponponda0103@gmail.com>
2026-03-25 21:01:20 +0900

    コマンドラインで受け取った値でfizzBuzz出力

--- main.rb
+++ main.rb
@@ -8,12 +8,23 @@ require_relative 'fizzBuzz'
 n = ARGV[0]
 
 
-puts n.class 
-puts n
+# puts n.class 
+# puts n
+
+args_int = n.to_i
+puts args_int.class
+puts "args_int: #{args_int}"
+
+# ".is_a?"はこのオブジェクトは○○型か？と確認するメソッドらしい
+# "?"をつけるのはtrue/falseを返すメソッドの命名規則らしい
+if args_int.is_a?(Integer)
+    fz = FizzBuzz.new
+    puts fz.fizz_buzz(15)
+else
+    puts "整数を入力してください"
+end
 
     # puts input.class 
     # # pochi = Hello.new("ぽち", 3)
     # # pochi.info
 
-    # fz = FizzBuzz.new
-    # puts fz.fizz_buzz(15)
