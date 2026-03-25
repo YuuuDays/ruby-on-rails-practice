4d9e350b0f982f4e4cab68e9d339d7c703099c4e
hikida <ponponda0103@gmail.com>
2026-03-25 20:44:54 +0900

    fizz-buzz 追加

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 require_relative 'hello'
+require_relative 'fizzBuzz'
 
-pochi = Hello.new("ぽち", 3)
-pochi.info
+
+# pochi = Hello.new("ぽち", 3)
+# pochi.info
+
+fz = FizzBuzz.new
+puts fz.fizz_buzz(15)
